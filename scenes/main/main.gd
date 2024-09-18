05ddef18b8aa70a43b776599c16101f9d5cd2474
justknick <126913469+justknick@users.noreply.github.com>
2024-09-18 18:45:18 -0400

    created game_over_ui scene, label for game_over // updated main scene, game scene, game_manager -- allows switching between main and game scenes

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,12 +1,20 @@
 extends Control
 
 @onready var bgm_sound: AudioStreamPlayer = $BGMSound
+@onready var start_label: Label = $MarginContainer/StartLabel
 
+var startable = false
 
 func _ready():
 	bgm_sound.play()
 
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("fly") == true:
-		GameManager.load_scene()
+func _process(_delta: float) -> void:
+	if startable == true:
+		if Input.is_action_just_pressed("fly") == true:
+			GameManager.load_game_scene()
+
+
+func _on_timer_timeout() -> void:
+	startable = true
+	start_label.visible = true
---
+++ scenes/ui/game_over_ui.gd
@@ -0,0 +1,32 @@
+extends Control
+
+@onready var game_over_label: Label = $MarginContainer/GameOverLabel
+@onready var space_label: Label = $MarginContainer/SpaceLabel
+@onready var timer: Timer = $Timer
+@onready var game_over_sound: AudioStreamPlayer = $GameOverSound
+
+
+func _ready() -> void:
+	hide()
+	SignalManager.on_plane_defeat.connect(on_defeat)
+
+
+func _process(delta: float) -> void:
+	check_input()
+
+
+func check_input() -> void:
+	if space_label.visible && Input.is_action_just_pressed("fly"):
+		GameManager.load_main_scene()
+
+
+func on_defeat():
+	show()
+	timer.start()
+	game_over_sound.play()
+
+
+func _on_timer_timeout() -> void:
+	#print("timeout")
+	game_over_label.hide()
+	space_label.show()
